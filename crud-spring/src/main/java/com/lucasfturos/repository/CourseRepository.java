38778e46123dad84d3ce9b027c8599c93104fbb8
lucasfturos <lucasfturos@gmail.com>
2024-04-24 18:37:43 -0300

    Update Repository

--- crud-spring/src/main/java/com/lucasfturos/repository/CourseRepository.java
+++ crud-spring/src/main/java/com/lucasfturos/repository/CourseRepository.java
@@ -1,11 +1,8 @@
 package com.lucasfturos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.lucasfturos.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
 }
