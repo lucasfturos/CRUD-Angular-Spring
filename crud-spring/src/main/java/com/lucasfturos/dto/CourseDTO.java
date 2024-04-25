304ed3346acf0cf2c7a26506468fb4faf4c7b62c
lucasfturos <lucasfturos@gmail.com>
2024-04-24 22:07:54 -0300

    Padrão DTO

---
+++ crud-spring/src/main/java/com/lucasfturos/dto/CourseDTO.java
@@ -0,0 +1,16 @@
+package com.lucasfturos.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO(
+        @JsonProperty("_id") Long id,
+        @NotNull @NotBlank @Length(min = 5, max = 100) String name,
+        @NotNull @Length(max = 10) @Pattern(regexp = "Back-End|Front-End") String category) {
+
+}
