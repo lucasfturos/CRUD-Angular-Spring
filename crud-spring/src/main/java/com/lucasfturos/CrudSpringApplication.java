2475e2a6796777cbdab7fe722c6dae6658b75bf4
lucasfturos <lucasfturos@gmail.com>
2024-04-29 16:20:26 -0300

    OneToMany

--- crud-spring/src/main/java/com/lucasfturos/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/lucasfturos/CrudSpringApplication.java
@@ -1,13 +1,38 @@
 package com.lucasfturos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.lucasfturos.enums.Category;
+import com.lucasfturos.model.Course;
+import com.lucasfturos.model.Lesson;
+import com.lucasfturos.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			var c = new Course();
+			c.setName("Angular");
+			c.setCategory(Category.FRONTEND);
+
+			var l = new Lesson();
+			l.setName("Introdução");
+			l.setVideoUrl("111");
+			l.setCourse(c);
+			c.getLessons().add(l);
+
+			courseRepository.save(c);
+		};
 	}
 
 }
--- crud-spring/src/main/java/com/lucasfturos/model/Course.java
+++ crud-spring/src/main/java/com/lucasfturos/model/Course.java
@@ -1,5 +1,8 @@
 package com.lucasfturos.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.validator.constraints.Length;
@@ -10,12 +13,14 @@ import com.lucasfturos.enums.Status;
 import com.lucasfturos.enums.converters.CategoryConverter;
 import com.lucasfturos.enums.converters.StatusConverter;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -46,5 +51,8 @@ public class Course {
     @Column(nullable = false)
     @Convert(converter = StatusConverter.class)
     private Status status = Status.ACTIVE;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
+    private List<Lesson> lessons = new ArrayList<>();
 
 }
