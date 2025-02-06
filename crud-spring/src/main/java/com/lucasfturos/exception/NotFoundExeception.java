7b399cb37c1e7663021c0ea54b086dca25d8105d
lucasfturos <lucasfturos@gmail.com>
2025-02-06 18:01:44 -0300

    Update spring

--- crud-spring/src/main/java/com/lucasfturos/exception/NotFoundExeception.java
+++ crud-spring/src/main/java/com/lucasfturos/exception/NotFoundExeception.java
@@ -1,7 +1,10 @@
 package com.lucasfturos.exception;
 
+import java.io.Serial;
+
 public class NotFoundExeception extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 1;
 
     public NotFoundExeception(Long id, String name) {
